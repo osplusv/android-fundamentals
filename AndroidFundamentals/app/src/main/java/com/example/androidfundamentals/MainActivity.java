aaaf4f72c998b2006506f0893d68a8fec408d2ef
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2016-02-22 12:02:04 -0600

    Addign code for Activity

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.androidfundamentals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = MainActivity.class.getName();
+    private int counter = 0;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(TAG, "onCreate");
+
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy");
+    }
+
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        Log.v(TAG, "onSaveInstanceState");
+//        outState.putInt("counter", counter);
+//        super.onSaveInstanceState(outState);
+//    }
+//
+//    // Called after onStart()
+//    @Override
+//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        Log.v(TAG, "onRestoreInstanceState");
+//        counter = savedInstanceState.getInt("counter");
+//    }
 }
