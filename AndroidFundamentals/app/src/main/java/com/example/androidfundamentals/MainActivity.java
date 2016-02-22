c0188c30d43434647436398b68f9f2fe0cd3615b
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2016-02-21 22:19:06 -0600

    Adding code for Button

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.androidfundamentals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text1, text2, text3, text4, text5, text6, text7, text8;
+    private AutoCompleteTextView autoCompleteTextView;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                CLASSES);
+
+        text1 = (TextView) findViewById(R.id.text1);
+        text2 = (TextView) findViewById(R.id.text2);
+        text3 = (TextView) findViewById(R.id.text3);
+        text4 = (TextView) findViewById(R.id.text4);
+        text5 = (TextView) findViewById(R.id.text5);
+        text6 = (TextView) findViewById(R.id.text6);
+        text7 = (TextView) findViewById(R.id.text7);
+        text8 = (TextView) findViewById(R.id.text8);
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        autoCompleteTextView.setAdapter(adapter);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String message = text1.getText().toString() + " " + text2.getText().toString() +
+                        " " + text3.getText().toString() + " " + text4.getText().toString() +
+                        " " + text5.getText().toString() + " " + text6.getText().toString() +
+                        " " + text7.getText().toString() + " " + text8.getText().toString() +
+                        " " + autoCompleteTextView.getText().toString();
+
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+    private static final String[] CLASSES = new String[] {
+      "Matemáticas", "Biologia", "Historia", "Android", "iOS", "Computación", "Español",
+            "Finanzas", "Química"
+    };
 }
