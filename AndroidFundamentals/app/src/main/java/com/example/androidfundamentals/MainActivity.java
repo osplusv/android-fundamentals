b0c2cfc088078d69baf272c244dc8d24e0d17be8
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2016-02-22 00:12:39 -0600

    Adding ActionBar actions

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.androidfundamentals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Uncomment to hide ActionBar
+         */
+        // android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        // actionBar.hide();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.compose:
+                showMessage("Compose");
+                return true;
+            case R.id.search:
+                showMessage("Search");
+                return true;
+            case R.id.settings:
+                showMessage("Settings");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
