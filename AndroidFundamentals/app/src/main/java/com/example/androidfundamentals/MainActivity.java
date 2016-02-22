63f53fbf8947944817850d31ba35a2bde32c5a6b
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2016-02-22 14:59:16 -0600

    Adding code for Fragments

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.androidfundamentals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private boolean isFragmentVisible = false;
+    private  MyFragment2 mf2 = new MyFragment2();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String buttonMessage = "";
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                if (isFragmentVisible) {
+                    fragmentTransaction.remove(mf2);
+                    buttonMessage = "Show fragment!";
+                } else {
+                    fragmentTransaction.add(R.id.fragment_container, mf2);
+                    buttonMessage = "Hide fragment!";
+                }
+                button.setText(buttonMessage);
+                fragmentTransaction.commit();
+                isFragmentVisible = !isFragmentVisible;
+            }
+        });
     }
 }
---
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MyFragment.java
@@ -0,0 +1,89 @@
+package com.example.androidfundamentals;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Osvaldo Villagrana on 2/22/16.
+ */
+public class MyFragment extends Fragment {
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    /**
+     * The system calls this when creating the fragment. Within your implementation, you should
+     * initialize essential components of the fragment that you want to retain when
+     * the fragment is paused or stopped, then resumed.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    /**
+     *The system calls this when it's time for the fragment to draw its user interface
+     * for the first time. To draw a UI for your fragment, you must return a View from
+     * this method that is the root of your fragment's layout.
+     * You can return null if the fragment does not provide a UI.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.example_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    /**
+     * The system calls this method as the first indication that the user is leaving the fragment
+     * (though it does not always mean the fragment is being destroyed). This is usually where you
+     * should commit any changes that should be persisted beyond the current user session
+     * (because the user might not come back).
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
