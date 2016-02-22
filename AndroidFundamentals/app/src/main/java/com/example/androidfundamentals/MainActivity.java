7ca945d101aec4cb78b223eb9dc49ee01b46f112
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2016-02-21 22:53:24 -0600

    Adding code for RadioButton

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.androidfundamentals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String message = "";
+    private RadioGroup genreRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        genreRadioGroup = (RadioGroup) findViewById(R.id.genreRadioGroup);
+        genreRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioFemale:
+                        message = "Female!";
+                        break;
+
+                    case R.id.radioMale:
+                        message = "Male!";
+                        break;
+                }
+                showMessage(message);
+            }
+        });
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        if (!checked) {
+            return;
+        }
+
+        switch (view.getId()) {
+            case R.id.radioYes:
+                message = "Yes :)";
+                break;
+
+            case R.id.radioMaybe:
+                message = "Maybe :/";
+                break;
+
+            case R.id.radioNo:
+                message = "No :(";
+                break;
+        }
+        showMessage(message);
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
