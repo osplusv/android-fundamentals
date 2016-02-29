55530fcfd12f94bd01abf51e6061afeef5d4ef10
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2016-02-29 17:53:53 -0600

    Rmoving unused variables

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private boolean isFragmentVisible = false;
-    private  MyFragment2 mf2 = new MyFragment2();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
