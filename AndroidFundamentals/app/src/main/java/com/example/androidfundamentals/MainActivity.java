e26617b660d3351b4b4d340fa1e5a707e5956056
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2016-03-28 11:36:33 -0600

    Adding code for media player

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.androidfundamentals;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button play = (Button) findViewById(R.id.play);
+        final Button pause = (Button) findViewById(R.id.pause);
+        final Button stop = (Button) findViewById(R.id.stop);
+
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ServiceStreaming.class);
+                intent.setAction("com.example.androidfundamentals.action.PLAY");
+                startService(intent);
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ServiceStreaming.class);
+                intent.setAction("com.example.androidfundamentals.action.PAUSE");
+                startService(intent);
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ServiceStreaming.class);
+                intent.setAction("com.example.androidfundamentals.action.STOP");
+                startService(intent);
+            }
+        });
     }
+
+
+
+//    private void play() {
+//        //http://streaming.radionomy.com/70s-Disco-Nights?lang=en-US%2cen%3bq%3d0.8%2ces%3bq%3d0.6
+//
+//        Uri.Builder uri = new Uri.Builder();
+//        uri.scheme("http")
+//                .authority("streaming.radionomy.com")
+//                .appendPath("70s-Disco-Nights")
+//                .appendQueryParameter("lang", "en-US%2cen%3bq%3d0.8%2ces%3bq%3d0.6");
+//        MediaPlayer mediaPlayer = new MediaPlayer();
+//
+//        try {
+//            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//            mediaPlayer.setDataSource(getApplicationContext(), uri.build());
+//            mediaPlayer.setOnPreparedListener(this);
+//            mediaPlayer.prepareAsync(); // prepare async to not block main thread
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        mediaPlayer.start();
+//    }
 }
---
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/ServiceStreaming.java
@@ -0,0 +1,185 @@
+package com.example.androidfundamentals;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+
+/**
+ * Created by Osvaldo Villagrana on 3/20/16.
+ */
+public class ServiceStreaming extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener {
+    public static final String ACTION_PLAY = "com.example.androidfundamentals.action.PLAY";
+    public static final String ACTION_PAUSE = "com.example.androidfundamentals.action.PAUSE";
+    public static final String ACTION_STOP = "com.example.androidfundamentals.action.STOP";
+    public static final int STOPPED_STATE = 2;
+    public static final int PREPARING_STATE = 3;
+    public static final int PLAYING_STATE = 1;
+    public static final int PAUSED_STATE = 1;
+    final int NOTIFICATION_ID = 1;
+
+    int state = STOPPED_STATE;
+
+    MediaPlayer mediaPlayer = null;
+    WifiManager.WifiLock wifiLock;
+    NotificationManager mNotificationManager;
+    Notification.Builder mNotificationBuilder = null;
+
+
+    @Override
+    public void onCreate() {
+        wifiLock = ((WifiManager) getSystemService(Context.WIFI_SERVICE))
+                .createWifiLock(WifiManager.WIFI_MODE_FULL, "mylock");
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String action = intent.getAction();
+
+        if (action.equals(ACTION_PLAY)) processPlayRequest();
+        else if (action.equals(ACTION_PAUSE)) processPauseRequest();
+        else if (action.equals(ACTION_STOP)) processStopRequest();
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        state = PLAYING_STATE;
+        setUpAsForeground("Playing");
+        startMediaPlayer();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        mediaPlayer.reset();
+        return false;
+    }
+
+    @Override
+    public void onDestroy() {
+        stopForeground(true);
+
+        if (mediaPlayer != null) {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        if (wifiLock.isHeld()) {
+            wifiLock.release();
+        }
+    }
+
+    private void createMediaPlayer() {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+            mediaPlayer.setOnPreparedListener(this);
+        } else {
+            mediaPlayer.reset();
+        }
+    }
+
+    private void processPlayRequest() {
+        if (state == STOPPED_STATE) {
+            play();
+        } else if (state == PAUSED_STATE) {
+            state = PLAYING_STATE;
+            setUpAsForeground("Radio Station");
+            startMediaPlayer();
+        }
+    }
+
+    private void startMediaPlayer() {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
+    }
+
+    private void releaseResources(boolean releaseMediaPlayer) {
+        stopForeground(true);
+
+        if (releaseMediaPlayer && mediaPlayer != null) {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        if (wifiLock.isHeld()) {
+            wifiLock.release();
+        }
+    }
+
+    void setUpAsForeground(String text) {
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0,
+                new Intent(getApplicationContext(), MainActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        // Build the notification object.
+        mNotificationBuilder = new Notification.Builder(getApplicationContext())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker(text)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle("RandomMusicPlayer")
+                .setContentText(text)
+                .setContentIntent(pi)
+                .setOngoing(true);
+        startForeground(NOTIFICATION_ID, mNotificationBuilder.build());
+    }
+
+    private void processPauseRequest() {
+        if (state == PLAYING_STATE) {
+            state = PAUSED_STATE;
+            mediaPlayer.pause();
+            releaseResources(false);
+        }
+    }
+
+    private void processStopRequest() {
+        if (state == PLAYING_STATE || state == PAUSED_STATE) {
+            state = STOPPED_STATE;
+            releaseResources(true);
+            stopSelf();
+        }
+    }
+
+    private void play() {
+        state = STOPPED_STATE;
+        releaseResources(false);
+
+        createMediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        Uri.Builder uri = new Uri.Builder();
+        uri.scheme("http")
+                .authority("streaming.radionomy.com")
+                .appendPath("70s-Disco-Nights")
+                .appendQueryParameter("lang", "en-US%2cen%3bq%3d0.8%2ces%3bq%3d0.6");
+        try {
+            mediaPlayer.setDataSource(getApplicationContext(), uri.build());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        state = PREPARING_STATE;
+        setUpAsForeground("loading...");
+        mediaPlayer.prepareAsync();
+        wifiLock.acquire();
+    }
+}
