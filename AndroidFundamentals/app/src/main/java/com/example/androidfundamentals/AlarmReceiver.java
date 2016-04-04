9b54d2a5e5f4179ad346fb8ae157425cacc0b21a
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2016-04-04 00:36:01 -0500

    Adding new code

---
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.example.androidfundamentals;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Osvaldo Villagrana on 4/3/16.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+    public static final String ACTION_ALARM = "alarm";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm showing", Toast.LENGTH_SHORT).show();
+    }
+}
--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.androidfundamentals;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private String ALARM_ACTION = AlarmReceiver.ACTION_ALARM;
+
+    private AlarmManager alarmManager;
+    private PendingIntent alarmIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+        final DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
+        final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+
+        Intent intentToFire = new Intent(MainActivity.this, AlarmReceiver.class);
+        alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intentToFire, 0);
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+//                Calendar calendar = new GregorianCalendar(datePicker.getYear(),
+//                        datePicker.getMonth(),
+//                        datePicker.getDayOfMonth(),
+//                        timePicker.getCurrentHour(),
+//                        timePicker.getCurrentMinute());
+
+                Calendar calendar = Calendar.getInstance();
+//                calendar.set(Calendar.HOUR, 12); // At the hour you wanna fire
+//                calendar.set(Calendar.MINUTE, 16); // Particular minute
+//                calendar.set(Calendar.SECOND, 0);
+
+                //Toast.makeText(getApplicationContext(), Double.toString(calendar.getTimeInMillis()), Toast.LENGTH_SHORT).show();
+
+                int alarmType = AlarmManager.RTC_WAKEUP;
+                alarmManager.setRepeating(alarmType, calendar.getTimeInMillis(), 30*1000, alarmIntent);
+            }
+        });
     }
 }
